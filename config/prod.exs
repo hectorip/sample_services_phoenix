5cdf27160fbd92f0eb471c645bead0c87631b9f3
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-02 14:18:06 -0600

    Production config from Heroku ENV variables

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :sample_services, SampleServices.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "floating-badlands-2133.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -58,4 +66,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
